5724ec4d4ae6f88f779f8f00318c0bc968f2704e
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2018-11-22 15:48:49 +0300

    start

---
+++ Linfo/Linfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Linfo
+//
+//  Created by Егор on 22/11/2018.
+//  Copyright © 2018 Егор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
