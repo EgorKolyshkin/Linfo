0cc965fcb64de64149b2d4ecc467f57bc99a664b
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2018-12-06 20:03:27 +0300

    Created skeleton of StreamersViewController и LiveStreamer Model

---
+++ Linfo/Linfo/Model/LiveStreamer.swift
@@ -0,0 +1,15 @@
+//
+//  LiveStreamer.swift
+//  Linfo
+//
+//  Created by Егор on 06/12/2018.
+//  Copyright © 2018 Егор. All rights reserved.
+//
+
+import Foundation
+
+struct LiveStreamer: Decodable {
+    let id: String
+    let name: String
+    let playing: Bool
+}
